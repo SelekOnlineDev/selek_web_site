e29cca6a36b12a3b6e679edc8a9a23e7ee4390e1
Selek <selek.inbox@gmail.com>
2026-09-06 18:31:57 +0800

    Fix MongoDB configuration and update SEO

--- selek-backend/server.js
+++ selek-backend/server.js
@@ -24,7 +24,7 @@ app.use(cors({
 
 // MongoDB connection
 
-const mongoUri = process.env.MONGODB_URI;
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.${process.env.DB_CLUSTER_ID}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoUri)
   .then(() => console.log("MongoDB connected successfully"))
